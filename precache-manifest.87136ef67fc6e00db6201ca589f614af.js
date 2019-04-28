9d6f134a20d0957788dee39545d3d5f6b3afeb6b
Ryan Yang <javathon@gmail.com>
2019-04-28 13:23:41 -0700

    Updates

---
+++ precache-manifest.87136ef67fc6e00db6201ca589f614af.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a0d33bdccabdf209973e3b51e0c1a08",
+    "url": "/index.html"
+  },
+  {
+    "revision": "df97b483b0f6661ec15d",
+    "url": "/static/css/main.e23fee82.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "df97b483b0f6661ec15d",
+    "url": "/static/js/main.d01b9098.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
