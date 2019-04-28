38bd49cbb17ad8a10d8397a2b1d4a227c95d6200
Ryan Yang <javathon@gmail.com>
2019-04-28 13:23:14 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+## info.boba.watch
+The source code for the info/FAQ page for [Boba Watch](https://boba.watch).
---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+        Initial commit
+        </div>
+    );
+}
+
+export default App;
