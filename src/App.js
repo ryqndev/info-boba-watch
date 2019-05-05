d1636bdcd438331652285e793f499910714ed5d1
Ryan Yang <javathon@gmail.com>
2019-05-04 18:26:27 -0700

    had a couple bugs

--- src/App.js
+++ src/App.js
@@ -4,9 +4,10 @@ import './App.css';
 import FAQHolder from './components/FAQHolder';
 import {Typography} from '@material-ui/core';
 
+function App() {
     return (
         <div className="App">
-            <div className="faq-title--holder" onClick={() => { window.location = 'https://boba.watch/'; }}>
+            <div className="faq-title--holder" onClick={() => {window.location = 'https://boba.watch/'} }>
                 <div className="faq-title--logo"></div>
                 <Typography variant="h1">boba watch</Typography>
             </div>
